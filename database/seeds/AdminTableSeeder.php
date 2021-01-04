aa2b3932a73ad838331d89d1472cae059f1b4873
zero06061999 <builoc191999@gmail.com>
2021-01-04 09:42:43 +0700

    Seeder

---
+++ database/seeds/AdminTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => '123456789',
+            'image' => 'admin.png',
+            'status' => '0',
+            'role_id' => '0',
+        ]);
+    }
+}
