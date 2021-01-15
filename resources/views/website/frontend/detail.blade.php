bb98107fca07f16387c0f84c783eb6ca87e49592
zero06061999 <builoc191999@gmail.com>
2021-01-15 14:40:20 +0700

    fix comment

--- resources/views/website/frontend/detail.blade.php
+++ resources/views/website/frontend/detail.blade.php
@@ -237,9 +237,10 @@
                         </div>
                         <!--Comments-->
                         <div class="comments-area">
-                            <h4 class="mb-30">{{ $post->comments->count() . trans('message.comment') }}</h4>
-                            @foreach ($post->comments as $comment)
-                                <div class="comment-list">
+                        <h3 class="mb-30">{{ $post->comments->count() . trans('message.comment') }}</h3>
+                            <div class="comment-list">
+                                @foreach ($post->comments as $comment)
+
                                     <div class="single-comment justify-content-between d-flex" id="listcomment">
                                         <div class="user justify-content-between d-flex">
                                             <div class="thumb">
@@ -260,8 +261,8 @@
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                            @endforeach
+                                @endforeach
+                            </div>
 
                             <!--comment form-->
                             @auth
